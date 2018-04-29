a421b8aee43f3f82f3c4bb1b571a59ff15998c58
linefeedse <tor-ake.fransson@linefeed.se>
2018-04-29 21:48:43 +0200

    Add simple but functioning data fetcher

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sunnylog-http
 Download production data from SMA Sunny Boy using the HTTP interface
+
+Set the environment variables `SMA_BASEURL` e.g. `http://192.168.1.23` and `SMA_PASSWORD` to the password for `User` before running this program.
---
+++ sunnylog.go
@@ -0,0 +1,92 @@
+package main
+
+// Fetch todays production from an SMA Sunny Boy via the HTTP interface
+// Print it on stdout for now
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loginToken(baseUrl string, user string, pwd string) string {
+	loginUrl := baseUrl + "/dyn/login.json"
+	values := map[string]string{"right": user, "pass": pwd}
+	jsonValue, _ := json.Marshal(values)
+	resp, err := http.Post(loginUrl, "application/json", bytes.NewBuffer(jsonValue))
+	check(err)
+
+	fmt.Println("login Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
+	var loginResult map[string]interface{}
+	err = json.Unmarshal(body, &loginResult)
+	check(err)
+
+	sidMap := loginResult["result"].(map[string]interface{})
+	return sidMap["sid"].(string)
+}
+
+func logout(baseUrl string, token string) {
+	logoutUrl := baseUrl + "/dyn/logout.json?sid=" + token
+	resp, err := http.Post(logoutUrl, "application/json", bytes.NewBuffer([]byte("")))
+	check(err)
+
+	fmt.Println("logout Status:", resp.Status)
+}
+
+func gettimestamp() int64 {
+	now := time.Now()
+	midnightUtc := fmt.Sprintf("%d-%02d-%02dT00:00:00-00:00",
+		now.Year(), now.Month(), now.Day())
+	form := "2006-01-02T15:04:05-07:00"
+	t2, e := time.Parse(form, midnightUtc)
+	check(e)
+	return t2.Unix()
+}
+
+func getlog(baseUrl string, timeFrom int64, timeTo int64, token string) {
+	logUrl := baseUrl + "/dyn/getLogger.json?sid=" + token
+	type LogRequest struct {
+		DestDev []int `json:"destDev"`
+		Key     int   `json:"key"`
+		TStart  int64 `json:"tStart"`
+		TEnd    int64 `json:"tEnd"`
+	}
+
+	//Log keys are hardcoded in SMA scripts.js
+	totWhOut5min := 28672
+	values := &LogRequest{
+		DestDev: make([]int, 0),
+		Key:     totWhOut5min,
+		TStart:  timeFrom,
+		TEnd:    timeTo}
+	jsonValue, e := json.Marshal(values)
+	check(e)
+	resp, err := http.Post(logUrl, "application/json", bytes.NewBuffer(jsonValue))
+	check(err)
+	fmt.Println("logger Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
+
+func main() {
+	baseUrl := os.Getenv("SMA_BASEURL")
+	password := os.Getenv("SMA_PASSWORD")
+	token := loginToken(baseUrl, "usr", password)
+	timeFrom := gettimestamp()
+	timeTo := time.Now().Unix()
+	getlog(baseUrl, timeFrom, timeTo, token)
+	logout(baseUrl, token)
+}
